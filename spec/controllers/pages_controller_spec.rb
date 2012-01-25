0b8791fd7f837ff3dab2916604b005ea251fedb0
Chris Barnhill <JazzcatCB@gmail.com>
2012-01-24 16:43:47 -0800

    Commiting latest changes.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
