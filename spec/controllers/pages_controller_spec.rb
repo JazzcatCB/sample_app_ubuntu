6db84b495664ac4a3dcc4c57c73d3874916e6f65
Chris Barnhill <JazzcatCB@gmail.com>
2012-01-24 17:53:24 -0800

    Added unique view content

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,12 @@ render_views
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+			:content => "| About")
+    end
   end
 
   describe "GET 'home'" do
@@ -15,12 +21,24 @@ render_views
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+			:content => "| Home")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+			:content => "| Contact")
     end
   end
 
